6dd559ea1586d1195cec4ddd5b06c676d9f0ab67
Behemoth39 <simme_jonsson@hotmail.com>
2022-10-18 11:05:12 +0200

    All menu items added via JS

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 const app = {
-    menuItems: document.getElementById("menu-items"),
+  menuItems: document.getElementById("menu-items"),
 };
-
-function fillMenu(post) {
-    let div1 = document.createElement("div");
-    let div2 = document.createElement("div");
-    app.menuItems.append(div1);
-    //app.menuItems.append(div1);
-    div1.append(div2);
-    //div2.classList.add("menu-head");
-    //div2.insertAdjacentHTML("beforeend", `<h3>${post.name} ${post.price} kr</h3>`);
-    div2.insertAdjacentHTML("beforeend", `<h3>${post.name}</h3>`);
-    div2.insertAdjacentHTML("beforeend", `<h3>${post.price} kr</h3>`);
-    div1.insertAdjacentHTML("beforeend", `<p>${post.descriptionSe}</p>`);
-    div1.insertAdjacentHTML("beforeend", `<p>${post.descriptionEn}</p>`);
-}
 
 fetch("menu.json")
   .then((res) => res.json())
   .then((data) => {
     data.forEach((post) => {
-      fillMenu(post);
+      let div1 = document.createElement("div");
+      let div2 = document.createElement("div");
+      let div3 = document.createElement("div");
+      let div4 = document.createElement("div");
+      app.menuItems.append(div1);
+      div1.append(div2);
+      div2.append(div3);
+      div3.append(div4);
+      div1.classList.add("col-sm-6");
+      div2.classList.add("card");
+      div2.classList.add("bg-dark");
+      div2.classList.add("text-light");
+      div3.classList.add("card-body");
+      div4.classList.add("h5");
+      div4.classList.add("mb-3");
+        if (post.halfPrice) {
+            div4.insertAdjacentHTML("beforeend", `<h3>${post.nameSE}</h3>`);
+            div4.insertAdjacentHTML("beforeend", `<h3>Halv ${post.halfPrice}kr / Hel ${post.price}kr</h3>`);
+        } else { 
+            div4.insertAdjacentHTML("beforeend", `<h3>${post.nameSE} ${post.price}kr</h3>`);
+        }
+      div3.insertAdjacentHTML("beforeend", `<p>${post.descSE}</p>`);
+      div3.insertAdjacentHTML("beforeend", `<h3>${post.nameEN}</h3>`);
+      div3.insertAdjacentHTML("beforeend", `<p>${post.descEN}</p>`);      
+      post.element = div1;
+      app.data = data;        
     });
   });
